7af67a83fac3a331dab24c011f8ce9f368b4d30f
prokitos <typada@yandex.ru>
2024-05-23 18:48:36 +0300

    grpc golang

--- grpc/grpcServer/internal/grpc/server.go
+++ grpc/grpcServer/internal/grpc/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"module/internal/grpc/generated"
 
 	"google.golang.org/grpc"
@@ -16,6 +17,17 @@ func Register(gRPC *grpc.Server) {
 }
 
 func (s *serverApi) Register(ctx context.Context, req *generated.RegisterRequest) (*generated.RegisterResponse, error) {
-	// panic("not implemented")
-	return &generated.RegisterResponse{UserId: 14643}, nil
+
+	// попытаюсь получить данные и вывод их в консоль
+	var temp string = req.GetEmail() + " " + req.GetPassword()
+	fmt.Println(temp)
+
+	// потом какие то вычисления, связь с бд
+
+	var result int = 0
+	result = len(temp) * len(req.GetEmail())
+
+	// вывод пользователю его юзер айди. по идее сюда должен возвращаться registerResponse, который заполнится внутри DAO
+	return &generated.RegisterResponse{UserId: (int64(result))}, nil
+
 }
